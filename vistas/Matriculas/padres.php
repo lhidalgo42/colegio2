521ba279ab08dcb4c9e83bcaa0bb6d58b676f243
Diego Valdivia <divaldivia@alumnos.ua.cl>
2013-10-18 23:44:02 -0300

    Revert "Cambio de Tabla a Spans"
    This reverts commit 633a3cadf6b707bfe973c4e6c286186856811a0d.

--- vistas/Matriculas/padres.php
+++ vistas/Matriculas/padres.php
@@ -1,31 +1,21 @@
-<div class="span12">
-	<div>
-		<div class="span2"><strong>Familia</strong></div>
-		<div class="span4"><input type="text" id="familia" disabled class="span3"></div>
-		<div class="span4"><strong>Apoderado Economico</strong></div>
-		<div class="span2">
-			<div class="btn-group" id="AEconomico" data-toggle="buttons-radio">
-		    	<button type="button" class="btn" onClick="clickButton(0)">Papa</button>
-		    	<button type="button" class="btn" onClick="clickButton(1)">Mama</button>
-		    </div>
-		</div>
-	</div>
-	<div>
-		<div class="span2"><strong>Padre</strong></div>
-		<div class="span4">
-			<a id="modalPAPA" role="button" onClick="MostrarPadre(1)" class="btn" data-toggle="modal" rut="" nombre="" apellido1="" 
-				apellido2="" fechaNac="" sexo="1" profesion="" direccion="" comuna="" email="" lugarTrabajo="" direccionTrabajo="" 
-				telefonos="" aEconomico="" >
-				Papa
-			</a>
-		</div>
-		<div class="span4"><strong>Madre</strong></div>
-		<div class="span2">
-			<a id="modalMAMA" role="button" onClick="MostrarPadre(0)" class="btn" data-toggle="modal" rut="" nombre="" apellido1="" 
-				apellido2="" fechaNac="" sexo="0" profesion="" direccion="" comuna="" email="" lugarTrabajo="" direccionTrabajo="" 
-				telefonos="" aEconomico="">
-				Mama
-			</a>
-		</div>
-	</div>
-</div>
+<table  border="1" class="table table-bordered">
+  <tr>
+    <td><strong>Familia</strong></td>
+     <td><input type="text" id="familia" disabled class="span3"></td>
+    <td><strong>Apoderado Economico</strong></td>
+    <td>
+  <div class="btn-group" id="AEconomico" data-toggle="buttons-radio">
+ <button type="button" class="btn" onClick="clickButton(0)">Papa</button>
+ <button type="button" class="btn" onClick="clickButton(1)">Mama</button>
+</div></td>
+
+
+  </tr>
+  <tr>
+    <td><strong>Padre</strong></td>
+    <td><a id="modalPAPA" role="button" onClick="MostrarPadre(1)" class="btn" data-toggle="modal" rut="" nombre="" apellido1="" apellido2="" fechaNac="" sexo="1" profesion="" direccion="" comuna="" email="" lugarTrabajo="" direccionTrabajo="" telefonos="" aEconomico="" >Papa</a></td>
+    <td><strong>Madre</strong></td>
+    <td><a id="modalMAMA" role="button" onClick="MostrarPadre(0)" class="btn" data-toggle="modal" rut="" nombre="" apellido1="" apellido2="" fechaNac="" sexo="0" profesion="" direccion="" comuna="" email="" lugarTrabajo="" direccionTrabajo="" telefonos="" aEconomico="">Mama</a></td>
+
+  </tr>
+</table>
