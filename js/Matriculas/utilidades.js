012a8740bb39786ac552efefdafba78766a7adf4
Leonardo Hidalgo <leontuna@gmail.com>
2013-10-23 04:42:22 -0300

    agrupamiento del codigo a js

--- js/Matriculas/utilidades.js
+++ js/Matriculas/utilidades.js
@@ -1,4 +1,114 @@
-  function curso(I){
+  	var actualizar = setInterval(function(){
+		var cuotas = +$('#cuotas').val();
+		var col = 0;
+		var mat = 0;
+		var cou = 0;
+		var alm = 0;
+		var deu = 0;
+		var totalC1 = 0;
+		var totalC2 = 0;
+		var totalCol = 0;
+		var	totalReal= 0;
+		for(var i=1; i< cuotas +1; i++)
+		{
+			total1 = 0;
+			total2 = 0;
+			col = +$('#Col'+i).val() + col;
+			mat = +$('#Mat'+i).val() + mat;
+			cou = +$('#Cou'+i).val() + cou;
+			alm = +$('#Alm'+i).val() + alm;
+			deu = +$('#Deu'+i).val() + deu;
+			totalC1 = +$('#Col'+i).val() + +$('#Mat'+i).val() + +$('#Cou'+i).val();
+			totalC2 = +$('#Alm'+i).val() + +$('#Deu'+i).val();
+			totalCol = totalC1 + totalC2;
+			totalReal = totalCol + totalReal
+			$('#Total'+i).html(totalCol);	   
+		}
+		$('#ColT').html(col);
+		$('#MatT').html(mat);
+		$('#CouT').html(cou);
+		$('#AlmT').html(alm);
+		$('#DeuT').html(deu);
+		$('#TotalT').html(totalReal);
+		$("#familia").val(""+$("#modalPAPA").attr("apellido1")+" "+$("#modalMAMA").attr("apellido1")+"");
+	},500);
+	
+function enviar(){
+var familia = new Array();
+familia[0] = $("#familia").val();
+familia[1] = $("#AEconomico").children(".active").html()
+var Papas = new Array();
+for (var i = 0; i < 2; i++){
+	 Papas[i] = new Array();
+	  if (i == 1){var Data = "PAPA";}
+	  if (i == 0){var Data = "MAMA";}
+	 Papas[i][0] = $("#modal"+Data).attr("rut");
+	 Papas[i][1] = $("#modal"+Data).attr("nombre");
+	 Papas[i][2] = $("#modal"+Data).attr("apellido1");
+	 Papas[i][3] = $("#modal"+Data).attr("apellido2");
+	 Papas[i][4] = $("#modal"+Data).attr("fechaNac"); 
+	 Papas[i][5] = $("#modal"+Data).attr("profesion");
+	 Papas[i][6] = $("#modal"+Data).attr("direccion");
+	 Papas[i][7] = $("#modal"+Data).attr("comuna");
+	 Papas[i][8] = $("#modal"+Data).attr("email");
+	 Papas[i][9] = $("#modal"+Data).attr("lugarTrabajo");
+	 Papas[i][10] = $("#modal"+Data).attr("direccionTrabajo");
+	 Papas[i][11] = $("#modal"+Data).attr("telefonos");  
+}
+  var alumnos = new Array();
+ for( var i = 0; i < 4; i++){
+   var b= i+1;
+   alumnos[i] = new Array();
+   alumnos[i][0] = $("#rut"+b).val(); 
+   alumnos[i][1] = $("#modalNino"+b).attr("nombre");
+   alumnos[i][2] = $("#modalNino"+b).attr("apellidop");
+   alumnos[i][3] = $("#modalNino"+b).attr("apellidom");
+   alumnos[i][4] = $("#modalNino"+b).attr("fechanac");
+   alumnos[i][5] = $("#modalNino"+b).attr("colegioanterior");
+   alumnos[i][7] = $("#Curso"+b).val(); 
+   alumnos[i][8] = $("#bolM"+b).val(); 
+   alumnos[i][9] = $("#montoM"+b).val(); 
+   alumnos[i][10] = $("#fechaM"+b).val(); 
+   alumnos[i][11] = $("#valeS"+b).val(); 
+   alumnos[i][12] = $("#modalC"+b).attr("tipo"); 
+   alumnos[i][13] = $("#modalC"+b).attr("nombre"); 
+   alumnos[i][14] = $("#modalC"+b).attr("valor"); 
+   alumnos[i][15] = $("#fechaS"+b).val(); 
+   alumnos[i][16] = $("#1sem"+b).val(); 
+   alumnos[i][17] = $("#2sem"+b).val(); 
+} 
+var cuotas = +$('#cuotas').val();
+var documentos = new Array();
+for(var i = 0;i < cuotas; i++){
+	var b = i + 1;
+	documentos[i] = new Array();
+	documentos[i][0] = $('#FechaBol'+b).val();
+	documentos[i][1] = $('#Col'+b).val();
+	documentos[i][2] = $('#Mat'+b).val();
+	documentos[i][3] = $('#Cou'+b).val();
+	documentos[i][4] = $('#Alm'+b).val();
+	documentos[i][5] = $('#Deu'+b).val();
+	documentos[i][6] = $('#FechaD'+b).val();
+	documentos[i][7] = $('#obs'+b).val();
+	documentos[i][8] = $("#modalP"+b).attr("tipo");
+	documentos[i][9] = $("#modalP"+b).attr("banco");
+	documentos[i][10] = $("#modalP"+b).attr("numero");
+	documentos[i][11] = $("#modalP"+b).attr("monto");
+	documentos[i][12] = $("#Nbol"+b).val();
+}
+$.ajax({
+  url: "../../ajax/enviar.php",
+  data: {familia:familia,Papas:Papas,alumnos:alumnos,documentos:documentos},
+  type: "POST",
+  beforeSend: function()
+  {
+  },
+  success: function( data ) {
+    $("#ayuda").html(data);
+  }
+});} 
+	
+	function curso(I){
     var curso = $('#Curso'+I).val();
 	if(curso == "Kinder"){
 	var valor=190000;
@@ -82,41 +192,6 @@ function beca(I){
 		return true;
 	}
 }
-function sumar(){//variable que suma la totalidad de las columnas del documento
-	var cuotas = +$('#cuotas').val();
-    var col = 0;
-    var mat = 0;
-    var cou = 0;
-    var alm = 0;
-    var deu = 0;
-    var totalC1 = 0;
-    var totalC2 = 0;
-    var totalCol = 0;
-    var	totalReal= 0;
-    for(var i=1; i< cuotas +1; i++)
-	{
-    	total1 = 0;
-    	total2 = 0;
-	   	col = +$('#Col'+i).val() + col;
-	   	mat = +$('#Mat'+i).val() + mat;
-	   	cou = +$('#Cou'+i).val() + cou;
-	   	alm = +$('#Alm'+i).val() + alm;
-	   	deu = +$('#Deu'+i).val() + deu;
-	   	totalC1 = +$('#Col'+i).val() + +$('#Mat'+i).val() + +$('#Cou'+i).val();
-	   	totalC2 = +$('#Alm'+i).val() + +$('#Deu'+i).val();
-	   	totalCol = totalC1 + totalC2;
-	   	totalReal = totalCol + totalReal
-	   	$('#Total'+i).html(totalCol);
-	   
-	}
-    $('#ColT').html(col);
-    $('#MatT').html(mat);
-    $('#CouT').html(cou);
-    $('#AlmT').html(alm);
-    $('#DeuT').html(deu);
-    $('#TotalT').html(totalReal);
-    
-}
 function clickButton(I){
 if(I ==0){
 $("#AEconomico").children("button:first").addClass("btn-success active");
@@ -125,18 +200,6 @@ if(I==1){
 $("#AEconomico").children("button:last").addClass("btn-success active");
 $("#AEconomico").children("button:first").removeClass("btn-success active");}	
 }
-function bodyfuntion()
-{
-sumar();
-$("#familia").val(""+$("#modalPAPA").attr("apellido1")+" "+$("#modalMAMA").attr("apellido1")+"");
-}
-function boleta(){
-	var boleta = parseFloat($("#bolM1").val());
-	$("#bolM2").val(boleta);
-	$("#bolM3").val(boleta);
-	$("#bolM4").val(boleta);
-}
-
 $('#valeS1').keyup(function(){
 	var vale = $("#valeS1").val();
 	$("#valeS2").val(vale);
@@ -150,3 +213,18 @@ $('#bolM1').keyup(function(){
 	$('#bolM3').val(boleta);
 	$('#bolM4').val(boleta);
 })
+function cambio(){
+  		var cuotas = parseFloat($('#cuotas').val());
+		var cuotasInc = parseFloat($("#cuotaInc").val());
+		if(cuotas >32){var cuotas = 32}	
+		$('#pagoCuotas').html("");
+		for(var i=1;i<cuotasInc+1;i++){
+			$('#pagoCuotas').append('<tr><td><input style="width:115px;" type="number" id="Nbol'+i+'"></td><td><input style="width:115px;" type="date" id="FechaBol'+i+'"></td><td style="width:100px;"><center><a id="modalP'+i+'" role="button" onClick="MostrarP('+i+')"class="btn" data-toggle="modal" tipo="" banco="" numero="" monto="">Seleccionar</a></center></td><!-- suma desde aqui--><td><input type="number" style="width:80px;" min="0"  value="0" id="Col'+i+'" disabled></td><td><input type="number" style="width:80px;" min="0" value="0" id="Mat'+i+'" disabled></td><td style="width:90px;"><input type="number" style="width:90px;" min="0" value="0" id="Cou'+i+'"></td><td><input type="number" style="width:60px;" min="0" value="0" id="Alm'+i+'" disabled ></td><td style="width:90px;"><input type="number" style="width:90px;" min="0" value="0"  id="Deu'+i+'" disabled></td><!-- hasta aqui --><td style="width:80px;"><center><div id="Total'+i+'">0</div></center></td><td><input type="date" style="width:115px;" id="fechaD'+i+'" disabled></td><td><input type="text" style="width:170px;" id="obs'+i+'"></td></tr>');
+		}
+		for(var i=cuotasInc+1;i<cuotas+cuotasInc+1;i++){
+			$('#pagoCuotas').append('<tr><td><input style="width:115px;" type="number" id="Nbol'+i+'"></td><td><input style="width:115px;" type="date" id="FechaBol'+i+'"></td><td style="width:100px;"><center><a id="modalP'+i+'" role="button" onClick="MostrarP('+i+')"class="btn" data-toggle="modal" tipo="" banco="" numero="" monto="">Seleccionar</a></center></td><!-- suma desde aqui--><td><input type="number" style="width:80px;" min="0"  value="0" id="Col'+i+'"></td><td><input type="number" style="width:80px;" min="0" value="0" id="Mat'+i+'"></td><td style="width:90px;"><input type="number" style="width:90px;" min="0" value="0" id="Cou'+i+'" disabled></td><td><input type="number" style="width:60px;" min="0" value="0" id="Alm'+i+'" ></td><td style="width:90px;"><input type="number" style="width:90px;" min="0" value="0"  id="Deu'+i+'"></td><!-- hasta aqui --><td style="width:80px;"><center><div id="Total'+i+'">0</div></center></td><td><input type="date" style="width:115px;" id="fechaD'+i+'" disabled></td><td><input type="text" style="width:170px;" id="obs'+i+'"></td></tr>');
+		}	
+				
+	}
+$('#cuotaInc').keyup(cambio)	
+$('#cuotas').keyup(cambio)
